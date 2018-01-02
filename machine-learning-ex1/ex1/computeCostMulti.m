795d424445bd500406f2b4044596713fd72be207
Anubhav Dhoot <adhoot@salesforce.com>
2018-01-01 16:26:20 -0800

    Normalization for ignoring first columen

--- machine-learning-ex1/ex1/computeCostMulti.m
+++ machine-learning-ex1/ex1/computeCostMulti.m
@@ -6,7 +6,7 @@ function J = computeCostMulti(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
@@ -14,7 +14,9 @@ J = 0;
 %               You should set J to the cost.
 
 
-
+h = X * theta;
+diff = h - y;
+J = 1 / ( 2 * m) * sum (diff .* diff);
 
 
 % =========================================================================
